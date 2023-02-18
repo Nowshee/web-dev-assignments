870d907cbb88dff53e907dca7bc5af75a33ed9c4
Nowsheen <100213600+Nowshee@users.noreply.github.com>
2023-02-18 10:59:30 +0530

    Update CardComponents.js

--- CardComponents.js
+++ CardComponents.js
@@ -2,16 +2,6 @@ import React from 'react'
 
 export default function CardComponents(props) {
   return (
-//     <div>
-//       <div class="border border-secondary">
-//   <img src="..." class="rounded-circle" alt="..."/>
-//   <div >
-//   <h3 class="card-title">{props.Name}</h3>
-//     <p class="card-text">{props.email}</p>
-//     <p>{props.contact}</p>
-//     </div>
-// </div>
-// </div>
 <>
 <div className="card"  style={{ height:320,width:300,padding:42, margin:32}}>
   <img src={props.images} class="rounded-circle" alt="..." style={{width:100}}/>
